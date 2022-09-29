ce0dd5ad8598a5ae460b2b36f15156b7bf3cee3e
Ilie Todirascu <ilie.todirascu@amdaris.com>
2022-09-29 17:52:56 +0300

    70%

--- DiningHall/Extensions/ExtensionMethods.cs
+++ DiningHall/Extensions/ExtensionMethods.cs
@@ -10,10 +10,20 @@ namespace DiningHall.Extensions
 {
     public static class ExtensionMethods
     {
-        public static void SendOrder(this Order order)
+        public static void SendOrder(this Order order,Waiter waiter)
         {
-            Utility.Client.PostAsJsonAsync("http://host.docker.internal:60500/LogInfo", new { Message = $"Dining Hall to Kitchen Table:{order.Table.Number} sent order of {Utility.GetItems(order)}" });
+            Utility.Client.PostAsJsonAsync("http://host.docker.internal:60500/LogInfo", new { Message = $"Hello, Table Nr:{order.Table.Number}. My name is {waiter.Name}. Let me send " +
+                $"your order of {Utility.GetItems(order)} Priority:{order.Priority}" });
             Utility.Client.PostAsJsonAsync("http://host.docker.internal:60000/AddOrder", order);
+        }
+        public static void ReturnOrder(this Order order, Waiter waiter)
+        {
+            Utility.Client.PostAsJsonAsync("http://host.docker.internal:60500/LogInfo", new
+            {
+                Message = $"{waiter.Name}: Sorry fo the wait. Order for Table Nr:{order.Table.Number} of {Utility.GetItems(order)} is finished. Max Wait: {order.MaxWait * 250} " +
+                $"It took {Math.Ceiling(DateTimeOffset.Now.Subtract(order.TimeOfCreation).TotalSeconds) * 250}"
+            });
+            Utility.FinishedOrders.TryDequeue(out _);
         }
     }
 }
